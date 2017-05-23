427919a5004d99c591c567abfcbe346e1d382bae
Mary <marysalemme1@gmail.com>
2017-05-23 17:37:23 +0000

    Student count added

--- directory.rb
+++ directory.rb
@@ -10,5 +10,7 @@ puts "Terminator"
 puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
-puts "Norman Bates"
 puts "Voldemort"
+print "Overall, we have "
+print 11
+puts " great students"
