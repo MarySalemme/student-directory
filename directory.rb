617d7a206e0a684f7cb420491e318308c2c1842c
Mary <marysalemme1@gmail.com>
2017-05-25 11:30:48 +0000

    Changed input_students method to ask for cohort

--- directory.rb
+++ directory.rb
@@ -1,23 +1,19 @@
 def input_students
-    puts "Please enter the name of the student."
+    puts "Please enter the name of the student:"
     puts "Hit return to finish"
     students = []
     name = gets.chomp
     while !name.empty? do
         if name.start_with?("V") && name.length < 12
-            puts "What are this student's hobbies?"
-            hobbies = gets.chomp
-            puts "What's the country of birth?"
-            country_of_birth = gets.chomp
-            puts "And the date of birth?"
-            date_of_birth = gets.chomp
-            students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: country_of_birth, date_of_birth: date_of_birth}
+            puts "Please enter their cohort:"
+            cohort = gets.chomp
+             students << {name: name, cohort: cohort == ""? "January" : cohort} # cohort = "January" is the default value
             puts "Now we have #{students.count} students."
-            puts "Please enter the name of the student."
+            puts "Please enter the name of the student:"
             name = gets.chomp
         else
-            puts "The student added does not qualify."
-            puts "Please enter the name of the student."
+            puts "The student added does not qualify"
+            puts "Please enter the name of the student:"
             name = gets.chomp
         end
         
