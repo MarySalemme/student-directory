35c05d34b5b77066778a0282e4a9a2d7f503806c
Mary <marysalemme1@gmail.com>
2017-05-25 09:18:20 +0000

    More informations added: hobbies, country and date of birth

--- directory.rb
+++ directory.rb
@@ -1,11 +1,17 @@
 def input_students
     puts "Please enter the names of the students"
-    puts "Hit return twice to finish"
+    puts "Hit return to finish"
     students = []
     name = gets.chomp
     while !name.empty? do
         if name.start_with?("V") && name.length < 12
-            students << {name: name, cohort: :november}
+            puts "What are this student's hobbies?"
+            hobbies = gets.chomp
+            puts "What's the country of birth?"
+            country_of_birth = gets.chomp
+            puts "And the date of birth?"
+            date_of_birth = gets.chomp
+            students << {name: name, cohort: :november, hobbies: hobbies, country_of_birth: country_of_birth, date_of_birth: date_of_birth}
             puts "Now we have #{students.count} students."
         else
             puts "The student added does not qualify"
@@ -22,6 +28,7 @@ end
 
 def print(students)
     # each_with_index added to print the number
+    
     students.each_with_index do |student, index|
         puts "#{index + 1}: #{student[:name]} (#{student[:cohort]} cohort)"
     end
